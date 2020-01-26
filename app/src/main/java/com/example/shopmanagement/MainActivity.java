de3228415806454fd87897979360c8f0b205d5c8
rottenBanana-45 <towsifhossain5120@gmail.com>
2020-01-26 13:33:20 +0600

    Edited by Towsif

--- app/src/main/java/com/example/shopmanagement/MainActivity.java
+++ app/src/main/java/com/example/shopmanagement/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.shopmanagement;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button toastButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toastButton = findViewById(R.id.toast);
+        toastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Welcome", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }

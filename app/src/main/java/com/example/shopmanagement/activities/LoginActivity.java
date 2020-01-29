e715848288ab1087f3428fff6372f068ae9d0c6b
Debasish2017331091 <debasish1998khulna@gmail.com>
2020-01-29 19:42:22 +0600

    Login Signup jamela ache

---
+++ app/src/main/java/com/example/shopmanagement/activities/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.example.shopmanagement.activities;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+import com.example.shopmanagement.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void OpenSignupPage(View view) {
+        startActivity(new Intent(LoginActivity.this,SignupActivity.class));
+    }
+}
--- app/src/main/java/com/example/shopmanagement/fragments/StockFragment.java
+++ app/src/main/java/com/example/shopmanagement/fragments/StockFragment.java
@@ -1,23 +1,37 @@
 package com.example.shopmanagement.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.shopmanagement.R;
+import com.example.shopmanagement.activities.LoginActivity;
 
 import java.util.zip.Inflater;
 
 public class StockFragment extends Fragment {
+
+    Button test;
+    View mView;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_stock,container,false);
+        mView = inflater.inflate(R.layout.fragment_stock,container,false);
+        test = mView.findViewById(R.id.testButton);
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getContext(), LoginActivity.class));
+            }
+        });
+        return mView;
     }
 }
